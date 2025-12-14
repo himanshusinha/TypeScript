e380e48a8ba9afe5c3e6a62c10c53b91afee8659
himanshusinha <himanshusinh011@gmail.com>
2025-12-14 20:13:23 +0530

    array

--- array.js
+++
@@ -1,7 +0,0 @@
-var student = ["john", "sam", "peter"];
-var marks = [80, 90, 60];
-var studArr = ["bruce", "tom"];
-studArr.push("tom");
-marks.push(80);
-console.log(studArr);
-var collegeName = ["john", "peter"];
--- bigint.js
+++
@@ -1,5 +0,0 @@
-var num = 9007199254740991n;
-var x = 1n;
-var y = 2n;
-console.log(num + x);
-console.log(num + y);
